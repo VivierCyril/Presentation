7e12fb5c1b20895951f24a2ea8000363cb28b555
Vivier Cyril <cyril.vivier2@afpa.fr>
2025-10-23 16:59:46 +0200

    test

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import 'reveal.js/dist/reveal.css'
+import './styles/style.css'
+import Reveal from "reveal.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Notes from "reveal.js/plugin/notes/notes.esm.js";
+
+// Initialisation de reveal.js
+
+const initReveal = () => {
+    let deck = new Reveal({
+        plugins: [Highlight, Markdown, Notes],
+    });
+    // const inIframe = document.referrer !== "";
+    // const transition = document.body.dataset.transition as Options["transition"] ?? "slide"
+    deck.initialize({
+        width: 1920,
+        height: 1080,
+        maxScale: 1,
+        // progress: inIframe,
+        // controls: inIframe,
+        hash: true,
+        // disableLayout: true,
+        // transition,
+        // display: "flex"
+    });
+}
+
+if (document.querySelector('.reveal')) {
+    initReveal()
+}
+
