e9d799f575f26a99d573429d6df175162ebbc072
Vivier Cyril <cyril.vivier2@afpa.fr>
2025-10-23 15:02:17 +0200

    Erreur build

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,24 @@ import { resolve } from 'path';
 export default defineConfig({
     server: {
         port: 3000,
-        open: 'src/index.html',
+        open: true,
     },
     base: '/Presentation/',
     build: {
         outDir: 'dist',
         emptyOutDir: true,
         rollupOptions: {
-            output: {
-                assetFileNames: "assets/[name][extname]",
-                chunkFileNames: "[name].js"
-            },
             input: {
                 main: resolve(__dirname, 'index.html'),
             },
-        },
+            output: {
+                // Laisse Vite gérer les noms des fichiers par défaut
+                // ou utilise :
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+            },
+        }
+
     },
 });
